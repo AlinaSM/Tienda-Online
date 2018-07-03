94f212d34d4848126d70d16c72797064beeb198f
Alina Salinas Mendoza <alinasalmen995@gmail.com>
2018-07-02 22:41:11 -0500

    Subiendo proyecto

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es-MX">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css\bootstrap.min.css">
+    <link rel="stylesheet" href="css\estilos.css">
+    <link rel="shortcut icon" href="">
+    <title>Aperture: Encuentra la que quieres</title>
+</head>
+<body>
+
+    <?php
+    include('pw3_cabecera.php');
+    include('pw3_carrusel.php');
+    ?>
+
+
+    <form action="" method="">
+        <input type="text">
+        <input type="submit">
+    </form>
+
+    <script src="js/jquery.slim.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ pw3_cabecera.php
@@ -0,0 +1,45 @@
+<header class="navbar  navbar-expand-lg  navbar-light  bg-light  color-cabecera" >
+        
+    <a class="navbar-brand" href="#">
+        <img class="logo-principal" src="img/icons/logo.png">
+    </a>
+
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+           
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Departamentos
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <a class="dropdown-item" href="#">Tecnologia</a>
+                <a class="dropdown-item" href="#">Libros</a>
+                <a class="dropdown-item" href="#">Instrumentos Musicales</a>
+            </div>
+        </li>
+        <li class="nav-item">
+
+        <form class="form-inline  my-2  my-lg-0  ">
+            <input class="form-control mr-sm-2" type="search" placeholder="Articulo que desea...">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+        </form>
+        </li>
+        </ul>
+    </div>
+        <a class="navbar-brand" href="#">
+            Iniciar Sesión
+        </a>
+        <a class="navbar-brand" href="#">
+            Registrarse
+        </a>
+        <a class="navbar-brand" href="#">
+            <img class="logo-principal" src="img/icons/cart-shop.png">
+        </a>
+    <div>
+
+    </div>
+</header>
